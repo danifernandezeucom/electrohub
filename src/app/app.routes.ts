301a1b0e703a66e4faa958a6fab64a83f31683e4
= <=>
2025-04-07 11:25:20 -0300

    eliminación de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,3 @@
 import { Routes } from '@angular/router';
-import {ProductComponent} from './pages/product/product.component';
-import {ProductDetailComponent} from './pages/product-detail/product-detail.component';
 
-export const routes: Routes = [
-  {
-    path: '',
-    component: ProductComponent,
-  },
-  {
-    path: 'product-detail/:id',
-    component:ProductDetailComponent,
-  }
-];
+export const routes: Routes = [];
